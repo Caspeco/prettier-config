f46074176f2223da45257cfdf4377e82b1a4e785
Marcus Otterström <marcus.otterstrom@caspeco.se>
2024-04-19 15:40:30 +0200

    Be explicit about all defaults

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+/**
+ * @type {import('prettier').Options}
+ */
 module.exports = {
 	// Overridden rules
 	useTabs: true,
 	printWidth: 110,
 
-	// Default rules from Prettier
+	// Default rules from Prettier 3.2.5
 	tabWidth: 2,
 	semi: true,
-	// wip
+	singleQuote: false,
+	quoteProps: "as-needed",
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	rangeStart: 0,
+	rangeEnd: Infinity,
+	// parser: undefined
+	// filepath: undefined,
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: "preserve",
+	htmlWhitespaceSensitivity: "css",
+	vueIndentScriptAndStyle: false,
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto",
+	singleAttributePerLine: false,
 };
